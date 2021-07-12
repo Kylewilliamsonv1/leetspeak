dc72ab3b387653639f08a7bafac184a15c642229
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-07-12 14:57:24 -0700

    test; swaps s for z except first character
    Co-authored-by: professional-pigeon <professional-pigeon@users.noreply.github.com>

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,11 +1,21 @@
+require ('pry')
+
 class String
   def leetspeak 
-    letters_array = split("")
+    letters_array = split("") ## [s,a,s,s,a]
+    counter = 0
     letters_array.each() do |letter| 
-      position = letters_array.index(letter)
+      # position = letters_array.index(letter))
       if (letter === "e")
-          letters_array[position] = "3"
+        letters_array[counter] ="3"
+      elsif (letter === "o")
+        letters_array[counter] ="0"
+      elsif (letter === "I")
+        letters_array[counter] ="1"
+      elsif (letter === "s") && (counter != 0) && (letters_array[counter -1] != " ")
+        letters_array[counter] ="z"
       end
+      counter = counter + 1
     end
     letters_array.join
   end
